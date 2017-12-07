49243dae0aedf29baf756111767ce146d9420d27
Chris <chris.scogin@dsisolutions.biz>
2017-12-07 16:48:26 -0600

    tutorial 01 goes over simple functionality of how to start using npyscreen

---
+++ npyscreen-tutorial/01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import npyscreen
+
+
+def simple_function():
+        pass
+
+
+if __name__ == "__main__":
+    npyscreen.wrapper_basic(simple_function)
+    print("This is a test app")
